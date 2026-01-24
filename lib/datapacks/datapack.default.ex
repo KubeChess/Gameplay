63db0da8cf1b58857699ec09a59057feddd9a63d
fDero <francescodero@outlook.it>
2026-01-24 19:37:32 +0100

    Code cleaning

--- lib/datapacks/datapack.default.ex
+++ lib/datapacks/datapack.default.ex
@@ -5,19 +5,14 @@ defmodule ClusterChess.Datapacks.Default do
 
             @impl ClusterChess.Datapacks.Behaviour
             def enforce(data) do
-                keys = struct(__MODULE__, %{})
-                |> Map.keys()
-                |> Enum.map(&to_string/1)
-                |> Enum.filter(&(&1 != "__struct__"))
-
-                results = Enum.map(keys, fn k -> {k, data[k], is_nil(data[k])} end)
-                IO.inspect(results, label: "Key checks")
-
                 not_nil? = fn k -> !is_nil(data[k]) end
-
-                if Enum.all?(keys, not_nil?),
-                    do: {:ok, data},
-                    else: {:error, "Invalid Datapacks"}
+                res = struct(__MODULE__, %{})
+                    |> Map.keys()
+                    |> Enum.map(&to_string/1)
+                    |> Enum.filter(&(&1 != "__struct__"))
+                    |> Enum.all?(not_nil?)
+                if res, do: {:ok, data},
+                    else: {:error, "Invalid Datapack"}
             end
 
             defoverridable enforce: 1
