6deabc44ab313b02cdaab1d8d85e286b3e54e29d
fDero <francescodero@outlook.it>
2026-02-19 00:57:35 +0100

    Add test cases for state management

---
+++ test/gameplay/state_test.exs
@@ -0,0 +1,43 @@
+defmodule ClusterChess.Gameplay.State.Test do
+
+    use ExUnit.Case
+
+    alias ClusterChess.Gameplay.State
+
+    @initial_state State.new(
+        60 * 10,          # 10 minutes
+        5,                # 5 seconds increment
+        "white_player",   # white player uid
+        "black_player"    # black player uid
+    )
+
+    @example_first_move_req %{
+        type: "game.domove",
+        from: {:e, 2},
+        to: {:e, 4},
+        uid: "white_player"
+    }
+
+    @example_second_move_req %{
+        type: "game.domove",
+        from: {:e, 7},
+        to: {:e, 5},
+        uid: "black_player"
+    }
+
+    test "State ok [correctly handles first move]" do
+        assert {:ok, _state} = State.apply_move(@initial_state, @example_first_move_req)
+    end
+
+    test "State ok [correctly handles first two moves]" do
+        move1 = @example_first_move_req
+        move2 = @example_second_move_req
+        assert {:ok, state_after_first_move_1} = State.apply_move(@initial_state, move1)
+        assert {:ok, _state} = State.apply_move(state_after_first_move_1, move2)
+    end
+
+    test "State ok [correctly rejects invalid move]" do
+        first_move_as_black = @example_second_move_req
+        assert {:error, _reason} = State.apply_move(@initial_state, first_move_as_black)
+    end
+end
--- test/gameplay/tracker_test.exs
+++ test/gameplay/tracker_test.exs
@@ -6,11 +6,27 @@ defmodule ClusterChess.Gameplay.Tracker.Test do
     alias ClusterChess.Commons.Sentinel
     alias ClusterChess.Gameplay.State
 
+    @messages_used_for_subscription 1
+
+    @initial_state State.new(
+        60 * 10,          # 10 minutes
+        5,                # 5 seconds increment
+        "white_player",   # white player uid
+        "black_player"    # black player uid
+    )
+
     @example_first_move_req %{
         type: "game.domove",
         from: {:e, 2},
         to: {:e, 4},
         uid: "white_player"
+    }
+
+    @example_second_move_req %{
+        type: "game.domove",
+        from: {:e, 7},
+        to: {:e, 5},
+        uid: "black_player"
     }
 
     @example_spectate_req %{
@@ -19,12 +35,34 @@ defmodule ClusterChess.Gameplay.Tracker.Test do
     }
 
     test "Tracker ok [spectators notified]" do
-        {:ok, tracker} = GenServer.start_link(Tracker, %State{})
+        {:ok, tracker} = GenServer.start_link(Tracker, @initial_state)
         {:ok, white_player} = GenServer.start_link(Sentinel, %{})
         {:ok, spectator} = GenServer.start_link(Sentinel, %{})
         Sentinel.impersonate_and_call(spectator, @example_spectate_req, tracker)
         Sentinel.impersonate_and_call(white_player, @example_first_move_req, tracker)
         inbox = Sentinel.impersonate_and_describe(spectator)
-        assert length(inbox.messages) == 1
+        assert length(inbox.messages) == @messages_used_for_subscription + 1
+    end
+
+    test "Tracker ok [spectators notified more then once]" do
+        {:ok, tracker} = GenServer.start_link(Tracker, @initial_state)
+        {:ok, white_player} = GenServer.start_link(Sentinel, %{})
+        {:ok, black_player} = GenServer.start_link(Sentinel, %{})
+        {:ok, spectator} = GenServer.start_link(Sentinel, %{})
+        Sentinel.impersonate_and_call(spectator, @example_spectate_req, tracker)
+        Sentinel.impersonate_and_call(white_player, @example_first_move_req, tracker)
+        Sentinel.impersonate_and_call(black_player, @example_second_move_req, tracker)
+        inbox = Sentinel.impersonate_and_describe(spectator)
+        assert length(inbox.messages) == @messages_used_for_subscription + 2
+    end
+
+    test "Tracker ok [spectators not notified on errors]" do
+        {:ok, tracker} = GenServer.start_link(Tracker, @initial_state)
+        {:ok, black_player} = GenServer.start_link(Sentinel, %{})
+        {:ok, spectator} = GenServer.start_link(Sentinel, %{})
+        Sentinel.impersonate_and_call(spectator, @example_spectate_req, tracker)
+        Sentinel.impersonate_and_call(black_player, @example_second_move_req, tracker)
+        inbox = Sentinel.impersonate_and_describe(spectator)
+        assert length(inbox.messages) == @messages_used_for_subscription + 0
     end
 end
